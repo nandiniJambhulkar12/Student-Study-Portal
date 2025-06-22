fc1e641d03616482570c92dada504c49ff4805ba
Nandini Jambhulkar12 <147521548+nandiniJambhulkar12@users.noreply.github.com>
2025-06-22 22:50:26 +0530

    Delete api directory

--- api/index.py
+++
@@ -1,9 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-from vercel_wsgi import handle_request
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studentstudyportal.settings")
-application = get_wsgi_application()
-
-def handler(request, context):
-    return handle_request(request, application)
